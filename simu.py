bd4b236c7a8393272c82ad9cdce6427744facebb
Yun Luo <ylramirocs@gmail.com>
2021-02-18 18:34:38 +0800

    Update simu.py

--- simu.py
+++ simu.py
@@ -12,14 +12,14 @@ from matplotlib import pyplot as plt
 # deal_unconnected(G)
 #
 
-for lam in tqdm.tqdm(range(0,11,1),desc="a:"):
-    for b in tqdm.tqdm(range(1,11,1),desc="b:"):
+for lam in tqdm.tqdm(range(0,11,2),desc="a:"):
+    for b in tqdm.tqdm(range(0,21,2),desc="b:"):
         args = []
         time = 0
         while time<100:
-            G = nx.barabasi_albert_graph(200, 5)
+            G = nx.nx.erdos_renyi_graph(200,0.05)
             deal_unconnected(G)
-            args.append([G, 0.2, 0.3, 0.4, 0.3, 1, lam*0.1, b, 0.5, 100,10000])
+            args.append([G, 0.2, 0.3, 0.4, 0.3, 0.05, lam*0.1, 10, 0.5, 100,10000,b*0.01])
 
             # graph2, p, alpha, beta, gamma, tau, lmd, b, c, round_num,t0
             time += 1
@@ -28,11 +28,11 @@ for lam in tqdm.tqdm(range(0,11,1),desc="a:"):
         private_op = [r0[0] for r0 in result]
         public_op = [r0[1] for r0 in result]
         fitness = [r0[2] for r0 in result]
-        with open('results_strong_ba.txt', 'a') as f:
+        with open('results_weak_er_de.txt', 'a') as f:
             f.write(str(sum(fitness)/len(fitness))+",")
             f.write(str(sum(private_op) / len(private_op)) + ",")
             f.write(str(sum(public_op) / len(public_op)) + ",")
-    with open('results_strong_ba.txt', 'a') as f:
+    with open('results_weak_er_de.txt', 'a') as f:
         f.write("\n")
 # graph2,p,alpha,beta,gamma,tau,lmd,b,c,round_num = args
 #     graph = graph2.copy()
